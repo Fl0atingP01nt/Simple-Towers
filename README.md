9c3fbada8ab5a015af3d3bcbddc450bf6752271d
Fl0atingP01nt <113318320+Fl0atingP01nt@users.noreply.github.com>
2025-07-04 15:25:23 +0800

    finish terrain gen *for now*

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# simple-towers
-A simple 3D tower defense game
+# Simple Towers
+A simple 3D tower defense game made in Godot. Everything here is made by me and I'm using this to practice making 3D games.
+
+### Things I've Done So Far:
+	- Terrain Gen (unfinished)
+	- Camera (will come back later)
+
+### Planned additions:
+	- Turrets
+	- Enemy Path Generation
+	- Enemies
+	- UI
--- src/terrain/TerrainGen.gd
+++ src/terrain/TerrainGen.gd
@@ -7,9 +7,15 @@ class_name TerrainGen
 @export var z_radius:int = 16;
 
 @export_category('Noise Settings')
-@export var init_seed:int; #initial seed
-@export var noise_type:FastNoiseLite.NoiseType = FastNoiseLite.TYPE_PERLIN; #noise type with perlin as default
-@export var freqency:float = 0.001; #how often blobs appear
+@export var init_seed:int; ##initial seed that doesn't even get used lmao
+@export var noise_type:FastNoiseLite.NoiseType = FastNoiseLite.TYPE_PERLIN; ##noise type with perlin as default. Due to the lack of brainpower, fractal and cellular generation does not work
+@export var freqency:float = 0.001; ##how often blobs appear
+@export var threshold:float = 0; ##the required value of the noise before it generates
+@export var block_id:int; 
+
+@export_category('Fill Settings')
+@export var fill:bool = false;
+@export var fill_block_id:int;
 
 var _debug_mesh_lib:MeshLibrary = load("res://assets/debug/debug_mesh_library.tres");
 
@@ -21,13 +27,20 @@ func clear_blocks(length:int, height:int, width:int):
 			for z in range(-width, width):
 				set_cell_item(Vector3i(x, y, z), -1);
 
-func generate_blocks(length:int, height:int, width:int, block_id:int, threshold:float, new_seed:int):
+func fill_blocks(length:int, width:int, height:int, block_id:int): #I honestly don't know the use case for this since the generate_blocks() has autofill implemented
+	for x in range(-length, length):
+		for y in range(-height, height):
+			for z in range(-width, width):
+				set_cell_item(Vector3i(x, y, z), block_id);
+
+func generate_blocks(length:int, height:int, width:int, block_id:int, threshold:float, new_seed:int, fill:bool = false, fill_block_id:int = -1):
 	terrain_noise.seed = new_seed;
 	for x in range(-length, length):
 		for y in range(-height, height):
 			for z in range(-width, width):
 				if terrain_noise.get_noise_2d(x, z) > threshold:
 					set_cell_item(Vector3i(x,y,z), block_id);
+				else: if fill: set_cell_item(Vector3i(x,y,z), fill_block_id); #funny nesting
 
 func _ready() -> void:
 	terrain_noise.noise_type = noise_type;
@@ -37,11 +50,11 @@ func _ready() -> void:
 	if mesh_library == null:
 		mesh_library = _debug_mesh_lib;
 	
-	generate_blocks(x_radius, y_radius, z_radius, 0, 0, randi());
+	generate_blocks(x_radius, y_radius, z_radius, block_id, 0, randi(), fill, fill_block_id);
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		clear_blocks(x_radius,y_radius,z_radius);
 
 	if Input.is_action_just_pressed("ui_accept"):
-		generate_blocks(x_radius, y_radius, z_radius, 0, 0, randi());
+		generate_blocks(x_radius, y_radius, z_radius, block_id, 0, randi(), fill, fill_block_id);
